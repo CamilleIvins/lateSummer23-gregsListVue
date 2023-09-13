b815362be7b6b0fba0c68d0ef6b0c43cc4287e38
Camille Ivins <camillemivins@gmail.com>
2023-09-13 14:57:03 -0600

    House card est

---
+++ src/pages/HouseDetailsPage.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+
+    </div>
+</template>
+
+<script>
+import { AppState } from '../AppState';
+import { computed, reactive, onMounted } from 'vue';
+import Pop from '../utils/Pop.js';
+import { housesService } from '../services/HousesService.js';
+import { useRoute, useRouter } from 'vue-router';
+import { logger } from '../utils/Logger.js';
+
+export default {
+    setup() {
+        return {}
+    }
+}
+</script>
+
+<style class="scss" scoped></style>
